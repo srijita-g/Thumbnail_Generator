a2aaba2da91a6a4e68b48dce34a4b177487c63e2
Srijita <srijitaghosh0530@gmail.com>
2026-05-13 02:29:48 +0530

    updated cors

--- Backend/main.py
+++ Backend/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://thumbnail-generator-pi-eight.vercel.app"
+        "https://thumbnaix.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
