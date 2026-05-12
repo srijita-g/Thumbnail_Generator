3bae7a2f3214857ab005e1d082e014b036c963fb
Srijita <srijitaghosh0530@gmail.com>
2026-05-13 01:48:57 +0530

    fix cors for vercel

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,8 @@
 import logging
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-
 from fastapi.middleware.cors import CORSMiddleware
+
 from database import create_tables
 from routes import router
 from auth_routes import router as auth_router
@@ -12,7 +12,6 @@ async def lifespan(app: FastAPI):
     create_tables()
     yield
 
-
 app = FastAPI(
     title="ThumbNex",
     lifespan=lifespan
@@ -20,10 +19,14 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://thumbnail-generator-bnefmm4ld-srijita-gs-projects.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 app.include_router(auth_router)
 app.include_router(router)
